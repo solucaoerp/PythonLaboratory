7af70a22c7ea615581d7e5221c5ee0ab435bda74
Charles Borges <solucao.erp@gmail.com>
2022-08-14 16:21:15 -0300

    restructuring

--- 001-learning/001-startTest/main.py
+++
@@ -1,8 +0,0 @@
-from IPython.display import IFrame
-from platform import python_version
-
-# Versão da Linguagem Python
-print('Versão da Linguagem Python usada neste Jupyter Notebook:', python_version())
-
-# Executando um vídeo embedded
-IFrame(src="https://www.youtube.com/embed/Jvf5y21ZqtQ", width="560", height="315")
